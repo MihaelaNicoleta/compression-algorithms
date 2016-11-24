57da449743db27a377df404e7f1bd60a1d86c1de
MihaelaNicoleta <raumiha21@gmail.com>
2016-11-24 17:23:46 +0200

    Add method to write the header for the compressed file

--- LZW/LZW.cs
+++ LZW/LZW.cs
@@ -14,9 +14,11 @@ namespace LZW
         private int freeze;
         private int index;
         private int noOfSymbols = 3;
+        List<int> encodedIndexes;
 
         public LZW() {
             symbolDictionary = new Dictionary<int, List<String>>();
+            encodedIndexes = new List<int>();
         }
 
         public LZW(int freeze, int index)
@@ -25,6 +27,7 @@ namespace LZW
             this.index = index;
             int limit = (int)Math.Pow(2, index) - 1;
             symbolDictionary = new Dictionary<int, List<String>>(limit);
+            encodedIndexes = new List<int>();
         }
 
 
@@ -35,22 +38,56 @@ namespace LZW
             FileInfo f = new FileInfo(fileToRead);
             int nbr = 8 * (int)f.Length;
 
-            fillDictionaryFixedPart(symbolDictionary);
+            //fillDictionaryFixedPart(symbolDictionary);
             List<String> list;
 
             String symbol = "";
             
+            list = new List<String>();
+            list.Add("A");
+            symbolDictionary.Add(0, list);
+            list = new List<String>();
+            list.Add("B");
+            symbolDictionary.Add(1, list);
+            list = new List<String>();
+            list.Add("C");
+            symbolDictionary.Add(2, list);
+            list = new List<String>();
+            list.Add("D");
+            symbolDictionary.Add(3, list);
+
+            int currentPosition = symbolDictionary.Count();
+
             while (nbr > 0)
             {
                 int character = bitReader.readNBits(8);
-                checkIfValueExists(symbolDictionary, "A");
 
+                var c = (char)character;
+                var charString = c.ToString();
+
+                if (checkIfValueExists(symbolDictionary, symbol+ charString))
+                {
+                    symbol = symbol + charString;
+                }
+                else
+                {
+                    list = new List<string>();
+                    list.Add(symbol + charString); //symbol
+                    list.Add(charString); //ultimul adaugat
+                    symbolDictionary.Add(currentPosition, list);
+                    symbol = charString;
+
+                    currentPosition++;
+                }
                 nbr -= 8;
             }
 
             bitReader.cleanUp();
 
+            String compressedFile = "File.ext." + ((freeze == 1) ? "f" : "e") + "l" + index + ".lzw";
+            BitWriter bitWriter = new BitWriter(compressedFile);
 
+            writeCompressedFile(symbolDictionary, bitWriter);
 
 
             return true;
@@ -85,7 +122,7 @@ namespace LZW
             {
                 list = symbol.Value;
 
-                if (list.Contains(value))
+                if (list[0] == value)
                 {
                     return true;
                 }
@@ -93,5 +130,20 @@ namespace LZW
 
             return false;
         }
+
+        private void writeCompressedFile(Dictionary<int, List<String>> symbolDictionary, BitWriter bitWriter)
+        {
+            writeheader();
+
+
+        }
+
+        private void writeheader(BitWriter bitWriter)
+        {
+            bitWriter.writeBit(freeze);
+            bitWriter.writeNBits(index, 4);
+        }
+
+
     }
 }
