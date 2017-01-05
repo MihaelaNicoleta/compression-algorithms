a52d1b0f0ad42511c2c3f05689ffb165206a54d5
MihaelaNicoleta <raumiha21@gmail.com>
2017-01-05 21:47:13 +0200

    Add RSA class for encryption

---
+++ RSAEncryption/RSA.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RSAEncryption
+{
+    class RSA
+    {
+
+        public Boolean encrypt(String fileToRead)
+        {
+            return true;
+        }
+
+        public Boolean decrypt(String fileToRead)
+        {
+            return true;
+        }
