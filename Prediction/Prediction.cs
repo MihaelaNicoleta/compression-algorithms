81846ebe30d582d6a95d8d92355bf2bfbe0a6166
MihaelaNicoleta <raumiha21@gmail.com>
2017-01-07 19:49:35 +0200

    Add prediction class and default functions

---
+++ Prediction/Prediction.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prediction
+{
+    class Prediction
+    {
+        private int predictionType;
+
+        public Prediction()
+        {
+
+        }
+
+        public Prediction(int predictionType)
+        {
+
+        }
+
+        public bool compress()
+        {
+
+            return true;
+        }
+
+        public bool decompress()
+        {
+
+
+            return true;
+        }
+    }
+}
