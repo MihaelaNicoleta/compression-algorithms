2de5d74e388bbd25121f086ed41ff36024dff634
MihaelaNicoleta <raumiha21@gmail.com>
2017-01-09 01:03:54 +0200

    Add Histogram class

---
+++ Prediction/Histogram.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prediction
+{
+    class Histogram
+    {
+
+        Bitmap histogramPicture;
+        double scaleValue;
+
+        public Histogram()
+        {
+
+        }
+
+        public Histogram(double scaleValue)
+        {
+            this.scaleValue = scaleValue;
+        }
+
+        public Bitmap drawHistogram(int[,] matrix)
+        {
+
+
+            return histogramPicture;
+        }
+
+
+    }
+}
